15bf82da27f953bb7ccabec04f5c753235f24cd0
Farhad435 <107310539+Farhad435@users.noreply.github.com>
2024-11-22 11:15:04 +0400

    Update update-score.js

--- api/update-score.js
+++ api/update-score.js
@@ -1,11 +1,10 @@
-import { getEdgeConfig } from '@vercel/edge-config';
+import { set } from '@vercel/edge-config';
 
 export default async function handler(req, res) {
   const { telegramId, score } = req.body;
 
   try {
-    const edgeConfig = await getEdgeConfig();
-    await edgeConfig.set(`score-${telegramId}`, score);
+    await set(`score-${telegramId}`, score);
     res.status(200).json({ message: 'Score updated successfully' });
   } catch (error) {
     res.status(500).json({ error: 'Failed to update score' });
