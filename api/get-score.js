10d244d6011136c48f18f81f4b615fbf9b7e6e28
Farhad435 <107310539+Farhad435@users.noreply.github.com>
2024-11-22 11:07:55 +0400

    Update get-score.js

--- api/get-score.js
+++ api/get-score.js
@@ -1,29 +1,13 @@
-import { BlobServiceClient } from "@azure/storage-blob";
-
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-
-const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-const containerClient = blobServiceClient.getContainerClient("clicker-scores");
+import { getEdgeConfig } from '@vercel/edge-config';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const { telegramId } = req.query;
+  const { telegramId } = req.query;
 
-    if (!telegramId) {
-      return res.status(400).json({ error: "Telegram ID is required" });
-    }
-
-    const blobClient = containerClient.getBlockBlobClient(telegramId);
-
-    try {
-      // Xalı al
-      const existingBlob = await blobClient.downloadToBuffer();
-      let score = existingBlob.length ? parseInt(existingBlob.toString()) : 0;
-      return res.status(200).json({ score });
-    } catch (error) {
-      return res.status(500).json({ error: "Failed to retrieve score" });
-    }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
+  try {
+    const edgeConfig = await getEdgeConfig();
+    const score = await edgeConfig.get(`score-${telegramId}`);
+    res.status(200).json({ score: score || 0 });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve score' });
   }
 }
